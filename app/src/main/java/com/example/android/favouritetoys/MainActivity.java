c141698c197f743f1fe045cb11c075a4287bd5fb
martha <m.v.serpak@gmail.com>
2020-02-01 16:45:58 +0200

    0

--- app/src/main/java/com/example/android/favouritetoys/MainActivity.java
+++ app/src/main/java/com/example/android/favouritetoys/MainActivity.java
@@ -3,29 +3,24 @@ package com.example.android.favouritetoys;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.android.favouritetoys.NetworkUtils.NetworkUtils;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mSearchBoxEditText;
-    TextView mUrlDisplaytextView;
+    TextView mUrlDisplayTextView;
     TextView mSearchResultsTextView;
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        /* Return true to display menu */
-        return true;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +29,70 @@ public class MainActivity extends AppCompatActivity {
 
         /*TextView toysListTextView = findViewById(R.id.tv_toy_names);*/
         mSearchBoxEditText = findViewById(R.id.et_search_box);
-        mUrlDisplaytextView = findViewById(R.id.tv_url_display);
+        mUrlDisplayTextView = findViewById(R.id.tv_url_display);
         mSearchResultsTextView = findViewById(R.id.tv_gitgub_search_results);
+    }
 
+    private void makeGithubSearchQuery() {
+        /*
+         * Within this method, build the URL with the text
+         * from the EditText and set the built URL to the TextView .
+         * */
+        String githubQuery = mSearchBoxEditText.getText().toString();
+        /*
+         * It pulls the githubQuery from the Edit Text, calls the
+         * buildUrl() method.
+         * */
+        URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
+        /* and displays the resulting URL in the UrlDisplayTextView. */
+        mUrlDisplayTextView.setText(githubSearchUrl.toString());
+
+        /* Call getResponseFromHttpUrl and display the results
+         * in mSearchResultsTextView.
+         */
+        /*String githubSearchResults = null;
+        try {
+            githubSearchResults =
+                    NetworkUtils.getResponseFromHttpUrl(githubSearchUrl);
+            mSearchResultsTextView.setText(githubSearchResults);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        new GitHubQueryTask().execute(githubSearchUrl);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class GitHubQueryTask extends AsyncTask<URL, Void, String> {
+        /*Override the doInBackground method
+            to perform the query. Return the results.
+            */
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String githubSearchResults = null;
+            try {
+                githubSearchResults =
+                        NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return githubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String githubSearchResults) {
+            if (githubSearchResults != null && !
+                    githubSearchResults.equals("")) {
+                mSearchResultsTextView.setText(githubSearchResults);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        /* Return true to display menu */
+        return true;
     }
 
     @Override
@@ -51,26 +107,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    void makeGithubSearchQuery(){
-        /* Within this method, build the URL with the text
-        from the EditText and set the built URL to the TextView */
-        String githubQuery = mSearchBoxEditText.getText().toString();
-        /* It pulls the githubQuery from the Edit Text, calls the
-        * buildUrl() method .*/
-        URL gitgubSearchUrl = NetworkUtils.buildUrl(githubQuery);
-        /* and displays the resulting URL in the UrlDisplayTextView. */
-        mUrlDisplaytextView.setText(gitgubSearchUrl.toString());
-    }
 }
-        /*Use the static ToyBox.getToyNames
-        method and store the names in a String array*/
-    //String[] toyNames = ToyBox.getToysNames();
 
-        /*Loop through each toy and
-        append the name to the TextView */
-
-/*   for ( String toyName : toyNames) {
-            toysListTextView.append(toyName + "\n\n\n");
-        }
-*/
 
