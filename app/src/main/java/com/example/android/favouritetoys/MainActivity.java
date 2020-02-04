82812409e392898b398dbca0af18f20e5e021e3e
martha <m.v.serpak@gmail.com>
2020-02-04 10:22:30 +0200

    0

--- app/src/main/java/com/example/android/favouritetoys/MainActivity.java
+++ app/src/main/java/com/example/android/favouritetoys/MainActivity.java
@@ -8,7 +8,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.favouritetoys.NetworkUtils.NetworkUtils;
@@ -21,16 +23,19 @@ public class MainActivity extends AppCompatActivity {
     EditText mSearchBoxEditText;
     TextView mUrlDisplayTextView;
     TextView mSearchResultsTextView;
+    TextView mErrorMessageTextView;
+    ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*TextView toysListTextView = findViewById(R.id.tv_toy_names);*/
         mSearchBoxEditText = findViewById(R.id.et_search_box);
         mUrlDisplayTextView = findViewById(R.id.tv_url_display);
         mSearchResultsTextView = findViewById(R.id.tv_gitgub_search_results);
+        mErrorMessageTextView = findViewById(R.id.tv_error_message);
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
     }
 
     private void makeGithubSearchQuery() {
@@ -58,14 +63,51 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }*/
+
         new GitHubQueryTask().execute(githubSearchUrl);
+    }
+
+    /**
+     * This method will make the View for the JSON data visible and
+     * hide the error message.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showJsonDataView() {
+        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * This method will make the error message visible and hide the JSON
+     * View.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showErrorMessage() {
+        /** First, hide the currently visible data */
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+        /** Then show the error */
+        mErrorMessageTextView.setVisibility(View.VISIBLE);
     }
 
     @SuppressLint("StaticFieldLeak")
     public class GitHubQueryTask extends AsyncTask<URL, Void, String> {
+        /**
+         * Override onPreExecute to set the
+         * loading indicator to visible
+         */
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
         /*Override the doInBackground method
-            to perform the query. Return the results.
-            */
+                    to perform the query. Return the results. */
         @Override
         protected String doInBackground(URL... urls) {
             URL searchUrl = urls[0];
@@ -81,9 +123,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String githubSearchResults) {
+            /** As soon as the loading is complete, hide the loading indicator */
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+
             if (githubSearchResults != null && !
                     githubSearchResults.equals("")) {
                 mSearchResultsTextView.setText(githubSearchResults);
+            } else {
+                showErrorMessage();
             }
         }
     }
