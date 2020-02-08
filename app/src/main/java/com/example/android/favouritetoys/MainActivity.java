68f4f2b502987e19dca18a85352c8c8911c73ad9
martha <m.v.serpak@gmail.com>
2020-02-08 15:44:52 +0200

    0

--- app/src/main/java/com/example/android/favouritetoys/MainActivity.java
+++ app/src/main/java/com/example/android/favouritetoys/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.favouritetoys;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final int NUM_LIST_ITEMS = 100;
 
     /* References to Recycler View and Adapter to reset the list to it's
@@ -16,6 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Using findViewById, we get a reference to our RecyclerView from xml. This allows us to
+         * do things like set the adapter of the RecyclerView and toggle the visibility. */
+        mNumberList = findViewById(R.id.rv_numbers);
+
+        /* A LinearLayoutManager is responsible for(ответственный за)
+         * measuring(измерение) and positioning item views within a
+         * RecyclerView into a linear list. This means that it can produce either a horizontal or
+         * vertical list depending on which parameter you pass in to the LinearLayoutManager
+         * constructor. By default, if you don't specify an orientation, you get a vertical list.
+         * In our case, we want a vertical list, so we don't need to pass in an orientation flag to
+         * the LinearLayoutManager constructor.*/
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mNumberList.setLayoutManager(layoutManager);
+        mNumberList.setHasFixedSize(true);
+
+        /* The GreenAdapter is responsible for displaying each item in the list. */
+        mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+
+        mNumberList.setAdapter(mAdapter);
 
     }
 
