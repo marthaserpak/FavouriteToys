599d71cbc8c7f71e7603e33328d5df79d7a184a9
martha <m.v.serpak@gmail.com>
2020-02-26 12:50:28 +0200

    Visualizer

--- app/src/main/java/com/example/android/favouritetoys/Visualizer/VisualizerActivity.java
+++ app/src/main/java/com/example/android/favouritetoys/Visualizer/VisualizerActivity.java
@@ -62,8 +62,13 @@ public class VisualizerActivity extends AppCompatActivity implements
         mVisualizerView.setShowTreble(sharedPreferences.getBoolean(getString(R.string.pref_show_treble_key),
                 getResources().getBoolean(R.bool.press_show_treble_default)));
         mVisualizerView.setMinSizeScale(1);
-        mVisualizerView.setColor(getString(R.string.pref_color_blue_value));
+        loadColorFromPreferences(sharedPreferences);
         sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    public void loadColorFromPreferences(SharedPreferences sharedPreferences){
+        mVisualizerView.setColor(sharedPreferences.getString(getString(R.string.pref_color_key),
+                getString(R.string.pref_color_red_value)));
     }
 
     /*
@@ -163,6 +168,8 @@ public class VisualizerActivity extends AppCompatActivity implements
         } else if (key.equals(getString(R.string.pref_show_treble_key))){
             mVisualizerView.setShowTreble(sharedPreferences.getBoolean(key,
                     getResources().getBoolean(R.bool.press_show_treble_default)));
+        } else if(key.equals(getString(R.string.pref_color_key))){
+            loadColorFromPreferences(sharedPreferences);
         }
     }
 }
