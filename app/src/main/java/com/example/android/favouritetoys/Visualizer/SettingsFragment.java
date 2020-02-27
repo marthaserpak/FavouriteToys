7de91c23a4ca39eb804ed98b5023f55859b5d538
martha <m.v.serpak@gmail.com>
2020-02-27 12:54:52 +0200

    Visualizer

--- app/src/main/java/com/example/android/favouritetoys/Visualizer/SettingsFragment.java
+++ app/src/main/java/com/example/android/favouritetoys/Visualizer/SettingsFragment.java
@@ -1,8 +1,11 @@
 package com.example.android.favouritetoys.Visualizer;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 import com.example.android.favouritetoys.R;
 
@@ -13,5 +16,15 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         //this method generate a preference screen from that file.
         addPreferencesFromResource(R.xml.pref_visualizer);
-    }
+
+        SharedPreferences sharedPreferences = getPreferenceScreen()
+                .getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        int count = preferenceScreen.getPreferenceCount();
+
+        for (int i = 0; i < count; i++){
+            Preference p = preferenceScreen.getPreference(i);
+        }
+
+    } 
 }
