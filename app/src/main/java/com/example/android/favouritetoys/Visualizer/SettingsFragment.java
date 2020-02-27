189da971bd96c40c5a2df122bd9a88c628f2327e
martha <m.v.serpak@gmail.com>
2020-02-27 15:07:59 +0200

    Summary Preference

--- app/src/main/java/com/example/android/favouritetoys/Visualizer/SettingsFragment.java
+++ app/src/main/java/com/example/android/favouritetoys/Visualizer/SettingsFragment.java
@@ -3,13 +3,17 @@ package com.example.android.favouritetoys.Visualizer;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
 
 import com.example.android.favouritetoys.R;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState,
                                     String rootKey) {
@@ -24,7 +28,48 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         for (int i = 0; i < count; i++){
             Preference p = preferenceScreen.getPreference(i);
+            if(!(p instanceof CheckBoxPreference)){
+                String value = sharedPreferences.getString(p.getKey(),
+                        "");
+                setPreferenceSummary(p, value);
+            }
         }
+    }
 
+    public void setPreferenceSummary(Preference preference,
+                                     String value) {
+        if(preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(value);
+            if(prefIndex >= 0){
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if(preference != null) {
+            if(!(preference instanceof CheckBoxPreference)){
+                String value = sharedPreferences.getString(
+                        preference.getKey(),"");
+                setPreferenceSummary(preference,value);
+            }
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
